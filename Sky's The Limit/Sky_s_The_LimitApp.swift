4fc6c64c2978b60af2a4c9096f5b90d3420ef57a
Liu Weimi Chris <notexttospeachfmsp@gmail.com>
2025-11-07 14:22:48 +0800

    Initial Commit

---
+++ Sky's The Limit/Sky_s_The_LimitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sky_s_The_LimitApp.swift
+//  Sky's The Limit
+//
+//  Created by Chris  on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Sky_s_The_LimitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
