77ae0d55492850afcc90f5df6d9c44fa0b897589
Liu Weimi Chris <notexttospeachfmsp@gmail.com>
2025-11-10 16:32:26 +0800

    rnoznriznfe ninfzifzni nzdenfozenoi nzeifnzifne izenf izefi neziofn zeifni zeifnz einf iznfioz nfi zfi izfbzjhebfjkzefzen flzefklzjkf nzaefbzjef zjefjl zneflb ze

--- Skys The Limit/ContentView.swift
+++ Skys The Limit/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        MainMenuView()
+        Text("hi")
+            .font(.custom("SpaceMono-Regular", size: 18))
             .task { @MainActor in
                 print("posting to db")
                 await post_to_database()
                 
                 print("querying")
                 await list_document_for_user()
+                for family in UIFont.familyNames {
+                    print("Family: \(family)")
+                    for name in UIFont.fontNames(forFamilyName: family) {
+                        print("  Font: \(name)")
+                    }
+                }
+
             }
     }
 }
