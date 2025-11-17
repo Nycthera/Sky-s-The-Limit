ec7f811b4b2258acd1c9f72466618d7495bed127
Nhavin Thirukkumaran <nhavint@gmail.com>
2025-11-17 14:14:21 +0800

    tutorial

--- Skys The Limit/frontend/tutorial/TutorialLevelOneView.swift
+++ Skys The Limit/frontend/tutorial/TutorialLevelOneView.swift
@@ -1,37 +1,38 @@
-////
-////  TutorialLevelOneView.swift
+//////
+//////  TutorialLevelOneView.swift
 ////  Skys The Limit
 ////
 ////  Created by Chris  on 17/11/25.
 ////
-//
-//import AVKit
-//import SwiftUI
-//struct TutorialLevelOneView: View {
-//    @Binding var isShowingTutorial : Bool
-//    
-//    private let VideoFiles = []
-//    
-//    var body : some View {
-//        VStack{
-//            TabView {
-//                ForEach(VideoFiles, id: \.self) { fileName in
-//                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: {fileName}, withExtension: "mp4")!))
-//                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: {fileName}, withExtension: "mp4")!))
-//                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: {fileName}, withExtension: "mp4")!))
-//                        .padding()
-//                }
-//            }
-//            .tabViewStyle(.page(indexDisplayMode: .always))
-//        }
-//    }
-////    Button("Done"){
-////        isShowingTutorial = false
-////    }
-////    .fontWeight(.semibold)
-////    .padding()
-////    .frame(maxWidth: .infinity)
-////    .background(Color.blue)
-////    .foregroundColor(.white)
-////    .cornerRadius(15)
-////    .padding()
+
+import AVKit
+import SwiftUI
+struct TutorialLevelOneView: View {
+    @Binding var isShowingTutorial : Bool
+    
+    private let VideoFiles = ["Recording-1", "Recording-2", "Recording-3"]
+    
+    var body : some View {
+        VStack{
+            TabView {
+                ForEach(VideoFiles, id: \.self) { fileName in
+                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "Recording-1", withExtension: "mp4")!))
+                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "Recording-2", withExtension: "mp4")!))
+                    VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "Recording-3", withExtension: "mp4")!))
+                        .padding()
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .always))
+        }
+    }
+    func Button("Done"){
+        isShowingTutorial = false
+    }
+    .fontWeight(.semibold)
+    .padding()
+    .frame(maxWidth: .infinity)
+    .background(Color.blue)
+    .foregroundColor(.white)
+    .cornerRadius(15)
+    .padding()
+}
