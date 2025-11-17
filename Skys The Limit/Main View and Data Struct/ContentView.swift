d0c87faa9ce4f17507a9b05369831e4d4cba8c49
Liu Weimi Chris <notexttospeachfmsp@gmail.com>
2025-11-17 09:47:20 +0800

    added collsap button help me

--- Skys The Limit/Main View and Data Struct/ContentView.swift
+++ Skys The Limit/Main View and Data Struct/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             FadeShowView()
         }
       
-        .navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
+        .navigationViewStyle(.stack)
     }
 }
